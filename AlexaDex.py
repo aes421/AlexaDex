bdf9a2439b5544c1ed7a07eaf1231907c494134e
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-09-05 21:59:27 -0500

    starting point for reading from pokeapi and added the requests library to the deployment package

--- AlexaDex.py
+++ AlexaDex.py
@@ -8,7 +8,7 @@ http://amzn.to/1LGWsLG
 """
 
 from __future__ import print_function
-
+import requests
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -50,13 +50,13 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Welcome to AlexaDex. " \
+                    "You can ask me for type information by saying, " \
+                    "Alexa, what type is Pikachu?"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "You can ask me for type information by saying, " \
+                    "Alexa, what type is Pikachu?"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -72,9 +72,20 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
+def get_pokemon_info(intent, session):
+    card_title = intent['name']
+    should_end_session = True
 
+    if 'pokemonName' in intent['slots']:
+        url = "http://pokeapi.co/api/v2/pokemon/" + intent['slots']['pokemonName']['value']
+        payload = ""
+        response = requests.request("GET", url, data=payload)
+
+        speech_output = response.json()
+        reprompt_text = "You can ask me for type information by saying, Alexa, what type is Pikachu?"
+
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
 
 def set_color_in_session(intent, session):
     """ Sets the color in the session and prepares the speech to reply to the
@@ -102,27 +113,6 @@ def set_color_in_session(intent, session):
                         "my favorite color is red."
     return build_response({}, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
 # --------------- Events ------------------
@@ -156,9 +146,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "TypeLookUp":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+        return get_pokemon_info(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
